e553e0031f796959122646e1d995c0cb006db795
zhaleh <zhalehelmi@gmail.com>
2024-05-05 23:24:31 +0330

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Category from "./components/categori/Category";
 
 const App = () => {
   return (
     <div>
-      <h3>react</h3>
+      <h3 className="wrapper bg-faded-dark">
+        <Header />
+        <Category />
+      </h3>
     </div>
   );
 };
---
+++ src/components/categori/Category.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+
+const Category = () => {
+  useEffect(() => {}, []);
+
+  return <div>Category</div>;
+};
+
+export default Category;
